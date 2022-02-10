0659b1ab547e826aeb9f06b8100fff4c284854b7
Leandro de Oliveira <leanots@gmail.com>
2022-02-10 19:23:19 -0300

    Modificando estrutura e implementando front da calculadora de imc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+
+      <div className="area-input">
+       <input
+        type="text"
+        placeholder="Peso em (Kg) Ex:90"
+        /> 
+
+       <input
+        type="text"
+        placeholder="Altura em (cm) Ex:180"
+        /> 
+
+        <button>
+          Calcular 
+        </button>
+      </div>
+
+      <h2>Seu imc foi 25, você está abaixo do peso!</h2>
     </div>
   );
 }
 
-export default App;
+
